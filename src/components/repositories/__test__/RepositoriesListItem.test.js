7f58f0b4ebaa59c0d27e7c57549fc4fbe36d4929
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-07-04 22:58:28 -0400

    update

---
+++ src/components/repositories/__test__/RepositoriesListItem.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import RepositoriesListItem from "../RepositoriesListItem";
+import { MemoryRouter } from "react-router-dom";
+
+function renderComponent() {
+  const repository = {
+    full_name: "facebook/react",
+    language: "JavaScript",
+    description: "A js library",
+    owner: "Facebook",
+    name: "React",
+    html_url: "https://github.com/facebook/react",
+  };
+  render(
+    <MemoryRouter>
+      <RepositoriesListItem repository={repository} />
+    </MemoryRouter>
+  );
+}
+
+it("should show a link to GitHub homepage for a given repository", () => {
+  renderComponent();
+});
